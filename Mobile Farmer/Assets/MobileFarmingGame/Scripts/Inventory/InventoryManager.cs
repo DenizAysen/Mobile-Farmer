125d297ff16adac2efb1da3ba34bdb3e095cdf76
Turgut Deniz Aysen <daysen@xenolia.com>
2023-06-07 00:39:54 +0300

    json files added

--- Mobile Farmer/Assets/MobileFarmingGame/Scripts/Inventory/InventoryManager.cs
+++ Mobile Farmer/Assets/MobileFarmingGame/Scripts/Inventory/InventoryManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class InventoryManager : MonoBehaviour
 {
     private Inventory _inventory;
+    private string _dataPath;
     void Start()
     {
-        _inventory = new Inventory();
+        _dataPath = Application.dataPath + "/inventoryData.txt";
+        //_inventory = new Inventory();
+        LoadInventory();
 
         CropTile.onCropHarvested += CropHarvestedCallBack;
     }
@@ -19,5 +23,30 @@ public class InventoryManager : MonoBehaviour
     private void CropHarvestedCallBack(CropType cropType)
     {
         _inventory.CropHarvestedCallBack(cropType);
+
+        SaveInventory();
+    }
+    private void LoadInventory()
+    {        
+        string data = "";
+
+        if (File.Exists(_dataPath))
+        {
+            data = File.ReadAllText(_dataPath);
+            _inventory = JsonUtility.FromJson<Inventory>(data);
+
+            if (_inventory == null)
+                _inventory = new Inventory();
+        }
+        else
+        {
+            File.Create(_dataPath);
+            _inventory = new Inventory();
+        }        
+    }
+    private void SaveInventory()
+    {
+        string data = JsonUtility.ToJson(_inventory, true);
+        File.WriteAllText(_dataPath, data);
     }
 }
